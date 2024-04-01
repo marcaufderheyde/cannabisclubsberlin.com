1bc6d778210cabf59e75a01f7932f60aa3dc4eb9
Fabrizio <fabiecatinella@gmail.com>
2024-04-01 17:35:52 +0100

    [CAN-21] Fix image URL not loading in clubs Problem Images no loading on clubs page due to /en | /de relative links
    Solution
    Removed root directory from middleware.
    Changed Club-listing component from html5 img tag to nextjs img tag
    Note
    N/A

--- app/[locale]/clubs/club-list.tsx
+++ app/[locale]/clubs/club-list.tsx
@@ -1,156 +1,157 @@
 import { useTranslations } from 'next-intl';
 import styles from './ClubCard.module.css';
 import Logo from '@/public/logo';
+import Image from 'next/image';
 
 const clubs = [
     {
-        "key": "club1",
-      "name": "CSC High Ground Berlin e.V.",
-      "description": "",
-      "imageUrl": "berlinBud1.webp",
-        "clubPageUrl": ""
+        key: 'club1',
+        name: 'CSC High Ground Berlin e.V.',
+        description: '',
+        imageUrl: '/berlinBud1.webp',
+        clubPageUrl: '',
     },
     {
-        "key": "club2",
-      "name": "Green Social Club e.V., im Norden von Berlin",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club2',
+        name: 'Green Social Club e.V., im Norden von Berlin',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club3",
-      "name": "Aero Cannabis Club e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club3',
+        name: 'Aero Cannabis Club e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club4",
-      "name": "Green Leaf Society e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club4',
+        name: 'Green Leaf Society e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club5",
-      "name": "420 Club Berlin",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club5',
+        name: '420 Club Berlin',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club6",
-      "name": "Bastardo CSC Berlin",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club6',
+        name: 'Bastardo CSC Berlin',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club7",
-      "name": "CSC Berlin e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club7',
+        name: 'CSC Berlin e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club8",
-      "name": "1000 Berlin 15 e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club8',
+        name: '1000 Berlin 15 e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club9",
-      "name": "Cannabis Social Club Köpenick",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club9',
+        name: 'Cannabis Social Club Köpenick',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club10",
-      "name": "CSC High on Earth e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club10',
+        name: 'CSC High on Earth e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club11",
-      "name": "CSC Home of Hemp Berlin e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club11',
+        name: 'CSC Home of Hemp Berlin e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club12",
-      "name": "Cannabis Social Club Prenzlauer Berg Berlin e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club12',
+        name: 'Cannabis Social Club Prenzlauer Berg Berlin e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club13",
-      "name": "Cannabis Social Club meridiem Brandenburgo et Berolina e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club13',
+        name: 'Cannabis Social Club meridiem Brandenburgo et Berolina e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club14",
-      "name": "Cannabis-Club Lammbock e. V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club14',
+        name: 'Cannabis-Club Lammbock e. V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club15",
-      "name": "Cannamo Cannabis Club e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club15',
+        name: 'Cannamo Cannabis Club e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club16",
-      "name": "High Society Cannabis Club e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club16',
+        name: 'High Society Cannabis Club e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club17",
-      "name": "Hype and Dope, Cannabis Club Berlin e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club17',
+        name: 'Hype and Dope, Cannabis Club Berlin e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club18",
-      "name": "Koala Cannabis Social Club Berlin e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club18',
+        name: 'Koala Cannabis Social Club Berlin e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club19",
-      "name": "We Love Weed (WLW) – Cannabis Social Club Berlin Kreuzberg e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club19',
+        name: 'We Love Weed (WLW) – Cannabis Social Club Berlin Kreuzberg e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club20",
-      "name": "Anbaufreunde Berlin Social Club e. V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
+        key: 'club20',
+        name: 'Anbaufreunde Berlin Social Club e. V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
     },
     {
-        "key": "club21",
-      "name": "Cannabis Club Lammbock e.V.",
-      "description": "",
-      "imageUrl": "",
-      "clubPageUrl": ""
-    }
-  ];
+        key: 'club21',
+        name: 'Cannabis Club Lammbock e.V.',
+        description: '',
+        imageUrl: '',
+        clubPageUrl: '',
+    },
+];
 
 export default function ClubsList() {
     const t = useTranslations('ClubsPage');
@@ -159,22 +160,24 @@ export default function ClubsList() {
     });
     return (
         <div className={styles.container}>
-          {clubs.map((club, index) => (
-            <div className={styles.card} key={index}>
-              <div className={styles.cardNumber}>#{index + 1}</div>
-              <img
-                src={club.imageUrl}
-                alt={club.name + " Club Picture"}
-                className={styles.cardImage}
-              />
-              <div className={styles.cardContent}>
-                <h3 className={styles.cardTitle}>{club.name}</h3>
-                <p className={styles.cardDescription}>
-                  {club.description}
-                </p>
-              </div>
-            </div>
-          ))}
+            {clubs.map((club, index) => (
+                <div className={styles.card} key={index}>
+                    <div className={styles.cardNumber}>#{index + 1}</div>
+                    <Image
+                        src={club.imageUrl}
+                        alt={club.name + ' Club Picture'}
+                        width={300}
+                        height={300}
+                        className={styles.cardImage}
+                    />
+                    <div className={styles.cardContent}>
+                        <h3 className={styles.cardTitle}>{club.name}</h3>
+                        <p className={styles.cardDescription}>
+                            {club.description}
+                        </p>
+                    </div>
+                </div>
+            ))}
         </div>
-      );
+    );
 }

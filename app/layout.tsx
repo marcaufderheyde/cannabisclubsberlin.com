646dc09418ec6b9f9603ab01fe4beaa6034d427c
Fabrizio <fabiecatinella@gmail.com>
2024-04-01 15:41:26 +0100

    [CAN-20] Hotfix - Metadata causes log errors Problem The metadata here was causing log errors
    Solution
    Deleted it. It's not needed right now.
    
    Note
    N/A

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Cannabis Clubs Berlin',
     description:
         'Your number one spot for all cannabis related information in Berlin, Germany!',
-    viewport: 'width=device-width,initial-scale=1',
 };
 
 export default function RootLayout({

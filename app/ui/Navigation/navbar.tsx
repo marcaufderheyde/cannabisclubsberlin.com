0d1e903ded69bc7d67f29dd05873b9d8497d4eee
Fabrizio <fabiecatinella@gmail.com>
2024-04-01 21:33:27 +0100

    [CAN-17] Lifted links up into NavBar Component Problem In order to create a hamburger, I need to lift links up to be reusable.
    Solution
    Remove It from Links component and move to
    Navbar component.
    
    Note

--- app/ui/Navigation/navbar.tsx
+++ app/ui/Navigation/navbar.tsx
@@ -1,13 +1,25 @@
 import Logo from '@/app/ui/Navigation/logo';
 import Links from '@/app/ui/Navigation/links';
 import TranslationSwitch from './translation-switch';
+import { useLocale, useTranslations } from 'next-intl';
+import LinkInfo from '@/app/ui/Navigation/linkinfo';
 
 export default function Navbar() {
+    const t = useTranslations('Navbar');
+    const localActive = useLocale();
+
+    const links: Array<LinkInfo> = [
+        { name: t('clubs_title'), href: `/${localActive}/clubs` },
+        { name: t('law_title'), href: `/${localActive}/law` },
+        { name: t('contact_title'), href: `/${localActive}/contact` },
+        { name: t('about_title'), href: `/${localActive}/about` },
+    ];
+
     return (
         <div className='text-white relative z-10 flex flex-row items-center justify-start my-4 gap-20'>
             <Logo />
             <div className='hidden md:flex md:flex-row items-center w-full justify-between'>
-                <Links />
+                <Links links={links} />
                 <TranslationSwitch />
             </div>
         </div>

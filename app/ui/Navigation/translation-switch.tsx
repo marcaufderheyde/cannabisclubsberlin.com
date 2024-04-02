53e1f1d239464409331fbe3cdef8be94bf9b276c
Fabrizio <fabiecatinella@gmail.com>
2024-04-02 03:12:40 +0100

    [CAN-22] Bugfix: Stop redirect to homepage Problem Using the translation switcher redirects user to homepage. We don't want that.
    Solution
    Turns out the issue is we are always just directing them to /[locale]
    So the solution is:
    - get current pathname and any params
    - remove the locale from pathname
    - reconstruct url
    - redirect using the router.
    
    Note
    N/A

--- app/ui/Navigation/translation-switch.tsx
+++ app/ui/Navigation/translation-switch.tsx
@@ -1,32 +1,46 @@
 'use client';
 
-import { useLocale } from "next-intl";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useTransition } from 'react';
+import { useLocale } from 'next-intl';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { ChangeEvent, useEffect, useTransition } from 'react';
 
 export default function LocalSwitcher() {
     const [isPending, startTransition] = useTransition();
     const router = useRouter();
     const localActive = useLocale();
-  
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+
+    const removeFirstDirectoryFromPathname = (path: string) => {
+        return path.replace(/^\/[^\/]+/, '');
+    };
+
+    const createPageURL = (local: string) => {
+        const params = new URLSearchParams(searchParams);
+        const pathnameWithoutCurrentLocal =
+            removeFirstDirectoryFromPathname(pathname);
+        return `/${local}${pathnameWithoutCurrentLocal}?${params.toString()}`;
+    };
+
     const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-      const nextLocale = e.target.value;
-      startTransition(() => {
-        router.replace(`/${nextLocale}`);
-      });
+        const nextLocale = e.target.value;
+        startTransition(() => {
+            const url = createPageURL(nextLocale);
+            router.replace(url);
+        });
     };
     return (
-      <label className='border-2 rounded'>
-        <p className='sr-only'>change language</p>
-        <select
-          defaultValue={localActive}
-          className='bg-transparent py-2'
-          onChange={onSelectChange}
-          disabled={isPending}
-        >
-          <option value='en'>English</option>
-          <option value='de'>German</option>
-        </select>
-      </label>
+        <label className='border-2 rounded'>
+            <p className='sr-only'>change language</p>
+            <select
+                defaultValue={localActive}
+                className='bg-transparent py-2'
+                onChange={onSelectChange}
+                disabled={isPending}
+            >
+                <option value='en'>English</option>
+                <option value='de'>German</option>
+            </select>
+        </label>
     );
-  }
+}

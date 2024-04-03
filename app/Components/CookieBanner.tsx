5dfdc34fb2b78e6b0853f0c3f5565ba82057c724
Fabrizio <fabiecatinella@gmail.com>
2024-04-03 18:32:12 +0100

    [No Tag] Hotfixes for Live Deploy Problem Banner shows up on refresh even after accepting cookies.
    Solution
    Tell the default state to be false not true until cookies have been checked
    Note
    THIS CAN BE FIXED WITH MIDDLEWARE! We just need a proper server.

--- app/Components/CookieBanner.tsx
+++ app/Components/CookieBanner.tsx
@@ -1,27 +1,30 @@
-"use client"
+'use client';
 import React, { useState, useEffect } from 'react';
 
 const CookieBanner: React.FC = () => {
-  const [showBanner, setShowBanner] = useState<boolean>(true);
+    const [showBanner, setShowBanner] = useState<boolean>(false);
 
-  useEffect(() => {
-    const isAccepted = localStorage.getItem('cookiesAccepted');
-    setShowBanner(!isAccepted);
-  }, []);
+    useEffect(() => {
+        const isAccepted = localStorage.getItem('cookiesAccepted');
+        setShowBanner(!isAccepted);
+    }, []);
 
-  const acceptCookies = () => {
-    localStorage.setItem('cookiesAccepted', 'true');
-    setShowBanner(false);
-  };
+    const acceptCookies = () => {
+        localStorage.setItem('cookiesAccepted', 'true');
+        setShowBanner(false);
+    };
 
-  if (!showBanner) return null;
+    if (!showBanner) return null;
 
-  return (
-    <div className="cookie-banner">
-      <p>We use cookies to improve your experience on our site. By continuing to use our site, you accept our use of cookies.</p>
-      <button onClick={acceptCookies}>Accept</button>
-    </div>
-  );
+    return (
+        <div className='cookie-banner'>
+            <p>
+                We use cookies to improve your experience on our site. By
+                continuing to use our site, you accept our use of cookies.
+            </p>
+            <button onClick={acceptCookies}>Accept</button>
+        </div>
+    );
 };
 
 export default CookieBanner;

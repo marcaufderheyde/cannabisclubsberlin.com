0523fe7f9068cf522f0a62f7febb9fc85737b203
marcaufderheyde <marcaufderheyde@gmail.com>
2024-04-04 21:13:40 +0200

    CAN-10 Subject: fix duplicated code in action button with cheeky elvis
    Problem: marc was duplicating code like a silly boyo
    
    Solution: ain't nothing but a hound dog

--- app/ui/Home/actionbutton.js
+++ app/ui/Home/actionbutton.js
@@ -8,29 +8,28 @@ export default function ActionButton({
     children,
     externalLink=false
 }) {
-    if(!externalLink) {
-        return (
-            <Link
-                href={href}
-                className={
+    return (
+        !externalLink ? 
+        <Link
+            href={href}
+            className={
+                'py-2 px-4 md:py-3 md:px-7 flex flex-row justify-center rounded-3xl cursor-pointer items-center gap-3'
+            }
+            style={{ color: textColor, backgroundColor: backgroundColor }}
+        >
+            {children}
+            <Triangle color={textColor} />
+        </Link> 
+        :
+        <a href={href} 
+            target="_blank" 
+            className={
                     'py-2 px-4 md:py-3 md:px-7 flex flex-row justify-center rounded-3xl cursor-pointer items-center gap-3'
                 }
                 style={{ color: textColor, backgroundColor: backgroundColor }}
-            >
+                >
                 {children}
                 <Triangle color={textColor} />
-            </Link>
-        );
-    }
-
-    return (
-        <a href={href} target="_blank" className={
-            'py-2 px-4 md:py-3 md:px-7 flex flex-row justify-center rounded-3xl cursor-pointer items-center gap-3'
-        }
-        style={{ color: textColor, backgroundColor: backgroundColor }}>
-
-            {children}
-            <Triangle color={textColor} />
         </a>
     );
 }

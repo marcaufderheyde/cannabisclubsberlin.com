735c370992737f570807c7cd6e3f9b45def2c49a
marcaufderheyde <marcaufderheyde@gmail.com>
2024-04-06 12:23:33 +0200

    CAN-40 Subject: changed how we store and load clubs
    Problem: was loading each club by copy and pasting, can be done with a helper function though
    
    Solution: wrote helper function and updated each club page
    
    Note: also made the whole card clickable on the clubslist

--- app/[locale]/clubs/cannabis-social-club-meridiem-brandenburgo-et-berolina-ev/clubs-content.tsx
+++ app/[locale]/clubs/cannabis-social-club-meridiem-brandenburgo-et-berolina-ev/clubs-content.tsx
@@ -1,45 +1,37 @@
 "use client"
 import { useLocale, useTranslations } from 'next-intl';
-import { generateSlug } from '../club-list';
-
-const club = {
-    key: 'club13',
-    name: 'Cannabis Social Club meridiem Brandenburgo et Berolina e.V.',
-    description: '',
-    offerings: '',
-    harm_reduction: '',
-    imageUrl: '/berlinBud1.webp',
-    clubPageUrl: '',
-    slug: "",
-};
-
-club.slug = generateSlug(club.name);
+import { usePathname } from 'next/navigation'
+import { pullClubsListContent } from '../clubsListContent';
 
 export default function ClubContent() {
+    const pathname = usePathname().split("/")[3];
+    const club = pullClubsListContent().find((club) => club.slug === pathname);
     const t = useTranslations('ClubsPage');
     const localActive = useLocale();
-    club.description = t(`${club.slug}.description`);
-    club.offerings = t(`${club.slug}.offerings`);
-    club.harm_reduction = t(`${club.slug}.harm_reduction`);
+    if(club) {
+        club.description = t(`${club.slug}.description`);
+        club.offerings = t(`${club.slug}.offerings`);
+        club.harm_reduction = t(`${club.slug}.harm_reduction`);
 
-    return (
-        <div className='flex flex-col gap-8 md:gap-12 md:mt-30 lg:mt-16'>
-            <h1 className='font-bold text-4xl md:text-[4rem] opacity-[0.3] text-balance leading-tight'>
-                {club.name}
-            </h1>
-            <h2 className='font-bold text-4xl md:text-[2rem] opacity-[0.3] text-balance leading-tight'>{t("description_title")}</h2>
-            <p>
-            {club.description}
-            </p>
-            <h2 className='font-bold text-4xl md:text-[2rem] opacity-[0.3] text-balance leading-tight'>{t("offerings_title")}</h2>
-            <p>
-            {club.offerings}
-            </p>
-            <h2 className='font-bold text-4xl md:text-[2rem] opacity-[0.3] text-balance leading-tight'>{t("harm_reduction_title")}</h2>
-            <p>
-            {club.harm_reduction}
-            </p>
+        return (
+            <div className='flex flex-col gap-8 md:gap-12 md:mt-30 lg:mt-16'>
+                <h1 className='font-bold text-4xl md:text-[4rem] opacity-[0.3] text-balance leading-tight'>
+                    {club.name}
+                </h1>
+                <h2 className='font-bold text-4xl md:text-[2rem] opacity-[0.3] text-balance leading-tight'>{t("description_title")}</h2>
+                <p>
+                {club.description}
+                </p>
+                <h2 className='font-bold text-4xl md:text-[2rem] opacity-[0.3] text-balance leading-tight'>{t("offerings_title")}</h2>
+                <p>
+                {club.offerings}
+                </p>
+                <h2 className='font-bold text-4xl md:text-[2rem] opacity-[0.3] text-balance leading-tight'>{t("harm_reduction_title")}</h2>
+                <p>
+                {club.harm_reduction}
+                </p>
 
-        </div>
-    );
+            </div>
+        );
+    }
 }

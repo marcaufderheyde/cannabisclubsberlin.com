340d32ba6835414724a111f56d756dbbd0a63caf
Hector Jones <jjones.hector@gmail.com>
2024-04-09 13:10:43 +0100

    [CAN-45] Adds on hover animation to desktop navbar links Problem Thought it would look a bit more alive to have active links with an animation upon hovering. Solution Thanks to this stackoverflow solution: https://stackoverflow.com/questions/70906977/tailwind-underline-hover-animation Note Very open to the styling being changed in the future, eg colour/size/animation etc.

--- app/ui/Navigation/links.tsx
+++ app/ui/Navigation/links.tsx
@@ -9,9 +9,10 @@ export default function Links({ links }: { links: Array<LinkInfo> }) {
                     <Link
                         key={link.name}
                         href={link.href}
-                        className='font-normal text-xl'
+                        className='font-normal text-xl group transition duration-300'
                     >
                         {link.name as String}
+                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-600"></span>
                     </Link>
                 );
             })}

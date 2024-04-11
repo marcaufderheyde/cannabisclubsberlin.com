ff913832d8bd8b0cabe998ce454ae10d3ec2209d
Fabrizio <fabiecatinella@gmail.com>
2024-04-11 18:14:32 +0100

    [CAN-58] Navbar changes colour on different pages Problem For the redesign we need to navbar to be grey instead of white for all pages except homepage
    Solution
    - create a helper function to match if pathname is the homepage
    - on navbar, conditional if homepage, text is white, otherwise it's grey
    - on links:
    - - if current page - bold text and make green
    - - if other page use default styling
    - - only if we're not on the homepage
    
    Note
    We will be removing the green curve on most pages hence the colour
    change.

--- app/ui/Navigation/links.tsx
+++ app/ui/Navigation/links.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link';
 import LinkInfo from '@/app/ui/Navigation/linkinfo';
+import { usePathname } from 'next/navigation';
+import isPathNameHome from '@/app/helpers/isPathnameHome';
 
 export default function Links({ links }: { links: Array<LinkInfo> }) {
+    const pathname = usePathname();
+    const currentPageStyling = 'text-[#E3E71F] font-bold text-xl';
+    const defaultLinkStyling = 'font-normal text-xl';
+    const isHomePage = isPathNameHome(pathname);
+
     return (
         <div className='flex flex-row justify-evenly flex-grow'>
             {links.map((link: LinkInfo) => {
@@ -9,7 +16,12 @@ export default function Links({ links }: { links: Array<LinkInfo> }) {
                     <Link
                         key={link.name}
                         href={link.href}
-                        className='font-normal text-xl'
+                        className={
+                            link.href.toString().includes(pathname) &&
+                            !isHomePage
+                                ? currentPageStyling
+                                : defaultLinkStyling
+                        }
                     >
                         {link.name as String}
                     </Link>

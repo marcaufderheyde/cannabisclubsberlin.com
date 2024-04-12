963da16d71a6045d26adb2ffd7d565268a3250c7
Fabrizio <fabiecatinella@gmail.com>
2024-04-12 12:29:58 +0100

    [CAN-59] Colour not showing on burger Problem On mobile view, the burger doesn't show any Colour
    Solution
    The reason is that the homepage has a coloured background,
    so no need for colour.
    Since removing it we need to give the burger a colour on every page that
    isn't the homepage.
    Note

--- app/ui/Navigation/hamburgerbutton.jsx
+++ app/ui/Navigation/hamburgerbutton.jsx
@@ -1,12 +1,20 @@
 import Burger from '@/app/ui/Navigation/burger';
+import isPathnameHome from '@/app/helpers/isPathnameHome';
+import { usePathname } from 'next/navigation';
 
 export default function HamburgerButton({ handleClick }) {
+
+    const pathname = usePathname();
+    const homeBurgerLineColor = "white";
+    const burgerLineColor = "rgba(182,207,84,1)";
+    
+
     return (
         <div
             onClick={() => handleClick()}
-            className='py-3 px-3 backdrop-blur-xl background-white rounded-[2rem] bg-[rgba(255,255,255,0.11)]'
+            className='py-3 px-3 backdrop-blur-xl rounded-[2rem] bg-[rgba(255,255,255,0.11)]'
         >
-            <Burger color={'white'} />
+            <Burger color={(isPathnameHome(pathname) ? homeBurgerLineColor : burgerLineColor)} />
         </div>
     );
 }

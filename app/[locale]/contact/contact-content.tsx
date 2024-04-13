a948565337db334bfee8969bab8d0cfd23a986e2
Fabrizio <fabiecatinella@gmail.com>
2024-04-13 14:24:32 +0100

    [CAN-48] Redesigned form Problem Form doesn't conform to new design
    Solution
    - Remove email
    - Reformat form to be more rounded and have extra padding
    
    Note

--- app/[locale]/contact/contact-content.tsx
+++ app/[locale]/contact/contact-content.tsx
@@ -7,14 +7,8 @@ export default function ContactContent() {
     const t = useTranslations('ContactPage');
     const localActive = useLocale();
     return (
-        <div className='flex flex-col gap-8 md:gap-12 md:mt-30 lg:mt-16'>
+        <div className='flex flex-col gap-8 md:gap-12 md:mt-30 lg:mt-16 w-full'>
             <PageHeader>{t('headline')}</PageHeader>
-            <p>{t('headline_description')}</p>
-            <p>
-                <a href={`mailto:${t('headline_subdescription')}`}>
-                    {t('headline_subdescription')}
-                </a>
-            </p>
             <ContactForm />
         </div>
     );

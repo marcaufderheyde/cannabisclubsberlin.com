3772a51574a68a102cb090602ad8d509f80eb0d0
Fabrizio <fabiecatinella@gmail.com>
2024-04-13 14:02:16 +0100

    [CAN-48] Refactor headers on pages to be re-usable Problem Lots of duplicate code just for header of page.
    Solution
    - Remove header from about
    - Place header from about into PageHeader Component
    - Place header into contact page
    
    Note

--- app/[locale]/about/about-content.tsx
+++ app/[locale]/about/about-content.tsx
@@ -1,5 +1,5 @@
 'use client';
-import GradientText from '@/app/Components/GradientText';
+import PageHeader from '@/app/Components/PageHeader';
 import ActionButton from '@/app/ui/Home/actionbutton';
 import { useLocale, useTranslations } from 'next-intl';
 
@@ -8,11 +8,7 @@ export default function AboutContent() {
     const localActive = useLocale();
     return (
         <div className='flex flex-col gap-8 md:gap-12 md:mt-30 lg:mt-16 justify-start items-start'>
-            <GradientText>
-                <h1 className='text-black font-bold text-4xl md:text-[4rem] text-balance leading-tight'>
-                    {t('headline')}
-                </h1>
-            </GradientText>
+            <PageHeader>{t('headline')}</PageHeader>
             <p className='text-xl md:text-3xl lg:text-4xl lg:leading-[2.875rem]  text-[#575757]'>
                 {t('headline_description_short')}
             </p>

cde8532196d5d3dda38836288e52be5e72e402e6
marcaufderheyde <marcaufderheyde@gmail.com>
2024-04-16 15:51:08 +0200

    CAN-65 - fix markers shifting location
    Problem: zooming caused markers to shift around map
    
    Solution: change the anchor position
    
    Note: also changed where popup starts, looks better

--- app/Components/OpenStreetMap.tsx
+++ app/Components/OpenStreetMap.tsx
@@ -15,10 +15,8 @@ const customIcon = L.icon({
   iconUrl: '/leaf-weed.png',
 
   iconSize:     [38, 38], // size of the icon
-  shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  iconAnchor:   [19, 37], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-3, -36] // point from which the popup should open relative to the iconAnchor
 });
 
 const OpenStreetMap = () => {

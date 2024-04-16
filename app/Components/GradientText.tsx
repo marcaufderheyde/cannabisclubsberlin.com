586ec54fef7286bda30c83d265b4257a1ef19358
Fabrizio <fabiecatinella@gmail.com>
2024-04-16 12:39:21 +0100

    [CAN-49] Accident - k/w git inside style Problem Git was in classname style Solution removed git keyword Note

--- app/Components/GradientText.tsx
+++ app/Components/GradientText.tsx
@@ -13,7 +13,7 @@ export default function GradientText({
                 return (
                     <div className='relative'>
                         {child}
-                        <div className='w-[6rem] md:w-[8rem] lg:w-[12rem] h-[1.625em] bg-gradient-to-r from-[#B6CF54] to-white absolute bottom-[-0.313em]  lg:bottom-[-0.3em] z-[-1] text-inherit'></div>
+                        <div className='w-[6rem] md:w-[8rem] lg:w-[12rem] h-[1.2em] md:h-[1.625em] bg-gradient-to-r from-[#B6CF54] to-white absolute bottom-[-0.313em]  lg:bottom-[-0.3em] z-[-1] text-inherit'></div>
                     </div>
                 );
             })}
--- app/[locale]/law/law-container.tsx
+++ app/[locale]/law/law-container.tsx
@@ -14,28 +14,26 @@ export default function LawContainer({
     const gridParentStyle = {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr 1fr 1fr',
-        gridTemplateRows: '1fr 1fr 1fr',
+        gridTemplateRows: 'auto',
         maxHeight: '500px',
         alignItems: 'Center',
     };
 
     return (
-        <div style={gridParentStyle}>
+        <div style={gridParentStyle} className='my-7'>
             <GradientText className='col-start-1 col-end-5 row-start-1 row-end-2'>
-                <h1 className='text-[1.5rem] md:text-[2.5rem] font-medium'>
+                <h1 className='text-[1.5rem] md:text-[2.5rem] font-medium align-end'>
                     {title}
                 </h1>
             </GradientText>
-            <p className='text-[1rem] md:text-[1.5rem] col-start-1 col-end-4 row-start-2 row-end-3 self-start'>
+            <p className='text-[1rem] my-5 md:text-[1.5rem] col-start-1 col-end-5 md:col-end-4 row-start-2 row-end-3 self-start'>
                 {text}
             </p>
 
-            <AutoScaler refScreenWidthInPixels='1920px'>
-                <SvgImage
-                    className='w-[176px] h-[176px] col-start-3 col-end-5 row-start-3 row-end-4 justify-self-center self-start'
-                    color='black'
-                ></SvgImage>
-            </AutoScaler>
+            <SvgImage
+                className='max-w-[100%] h-[90px] md:h-[176px] col-start-3 col-end-5 row-start-3 row-end-4 justify-self-center self-start'
+                color='#B6CF54'
+            ></SvgImage>
         </div>
     );
 }

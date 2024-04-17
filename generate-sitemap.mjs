7730b4f18f878ef8d119b1904e33e278e82a634b
Hector Jones <jjones.hector@gmail.com>
2024-04-17 12:42:59 +0100

    [CAN-69] Fixes sitemap generation script to work with directory Problem There were issues getting the globby package to find the required files in the project directory. As well as adding the hreflang rel attributes to each url. Solution The solution, based on: https://dev.to/valse/create-sitemaps-for-a-multi-language-multi-domain-url-translated-static-nextjs-site-41ap has successfully created two localised sitemaps, as well as a sitemap index. Note This still needs to be tested on our deployed website, using the tool listed on the Google Search docs (under 'debugging hreflang errors): https://developers.google.com/search/docs/specialty/international/localized-versions https://technicalseo.com/tools/hreflang/

---
+++ generate-sitemap.mjs
@@ -0,0 +1,98 @@
+import { writeFileSync } from 'fs';
+import { globby } from 'globby';
+import prettier from 'prettier';
+import path from 'path'; // Import path module to construct paths
+
+async function generate() {
+    const siteRootURL = 'https://cannabisclubsberlin.com'
+    const languages = ['de', 'en']; // Define languages
+    for (const lang of languages) {
+        const pages = await globby([
+            // Adjusted paths to match the directory structure
+            `.next/server/app/${lang}/**/*.html`,
+            `!.next/server/app/${lang}/_not-found.html`, // Exclude specific files
+            `!.next/server/app/${lang}/index.html`,
+            `.next/server/app/${lang}/clubs/*.html`, // Include club pages
+        ]);
+
+        const siteUrl =
+            lang === 'de'
+                ? `${siteRootURL}/de`
+                : `${siteRootURL}/en`;
+
+        // use otherLanguage to create alternates, with hreflang rel
+        const otherLanguage = lang =='de' ? 'en' : 'de';
+        const alternativeSiteUrl =
+        otherLanguage === 'de'
+                ? `${siteRootURL}/de`
+                : `${siteRootURL}/en`;
+
+        let sitemap = `
+                <?xml version="1.0" encoding="UTF-8"?>
+                <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                    <url>
+                        <loc>${siteUrl}</loc>
+                        <xhtml:link
+               rel="alternate"
+               hreflang="${otherLanguage}"
+               href="${alternativeSiteUrl}"/>
+                        <lastmod>${new Date().toISOString()}</lastmod>
+                    </url>
+                `;
+
+        for (const page of pages) {
+            const route = page
+                .replace(`.next/server/app/${lang}`, '')
+                .replace('.html', '');
+
+            const pageContent = `<url>
+                        <loc>${siteUrl}${route}</loc>
+                        <xhtml:link
+               rel="alternate"
+               hreflang="${otherLanguage}"
+               href="${alternativeSiteUrl}${route}"/>
+                        <lastmod>${new Date().toISOString()}</lastmod>                  
+                    </url>
+                    `;
+
+            sitemap += pageContent;
+        }
+
+        sitemap += `</urlset>`;
+
+        // console.log('Generated Localised Sitemap:', sitemap); // Debugging statement
+        console.log(`Generated Localised Sitemap ${lang}`);
+
+        const formatted = await prettier.format(sitemap, {
+            parser: 'html',
+        });
+
+        writeFileSync(path.join('sitemaps', `sitemap-${lang}.xml`), formatted); // Use path.join to construct file path
+    }
+
+    // generate sitemap index file here
+    let sitemapIndex = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <sitemap>
+        <loc>${siteRootURL}/sitemap-en.xml</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </sitemap>
+      <sitemap>
+        <loc>${siteRootURL}/sitemap-de.xml</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </sitemap>
+    </sitemapindex>
+                `;
+    // console.log('Generated Sitemap Index:', sitemapIndex); // Debugging statement
+    console.log('Generated Sitemap Index');
+
+    const formatted = await prettier.format(sitemapIndex, {
+        parser: 'html',
+    });
+
+    writeFileSync(path.join('sitemaps', `sitemap.xml`), formatted);
+
+}
+
+generate();

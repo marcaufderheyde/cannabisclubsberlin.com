f355d519dee140a956c17ab9ae0fd3914027e0cf
marcaufderheyde <marcaufderheyde@gmail.com>
2024-04-18 18:01:34 +0200

    CAN-81 - Example of how Metadata can be generated for inner club pages for 420-club-berlin
    Problem: tricky to get to what we need, hardcoded slugs are ugh
    
    Solution: use hardcoded slugs till we figure it out
    
    Note: also refactored clubs/page.tsx so that metadata can be generated from there, however, back button from club page to list/map seems to break something?

--- app/[locale]/clubs/420-club-berlin/page.tsx
+++ app/[locale]/clubs/420-club-berlin/page.tsx
@@ -1,5 +1,23 @@
 import ClubContent from '../club-content';
-import { unstable_setRequestLocale } from 'next-intl/server';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+import { pullClubsListContent } from '../clubsListContent';
+
+export const generateMetadata = async () => {
+    const club = pullClubsListContent().find(
+        (club) => club.slug === '420-club-berlin'
+    );
+    const t = await getTranslations('ClubsPage');
+    if (club) {
+        club.description = t(`${club.slug}.description`);
+        club.offerings = t(`${club.slug}.offerings`);
+
+        return {
+            title: club.name,
+            description: club.description,
+            keywords: club.offerings,
+        };
+    }
+};
 
 export default function Clubs({ params: { locale } }: { params: { locale: string } }) {
     unstable_setRequestLocale(locale);
--- app/[locale]/clubs/page.tsx
+++ app/[locale]/clubs/page.tsx
@@ -1,42 +1,25 @@
-'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import ClubsContent from './clubs-content';
-import ClubsList from './club-list';
-import dynamic from 'next/dynamic';
-import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
+
+export const generateMetadata = async () => {
+    const t = await getTranslations('Metadata');
+    return {
+        title: t('clubs_title'),
+        description: t('clubs_description'),
+        keywords: t('clubs_keywords'),
+    };
+};
 
 export default function Clubs({
     params: { locale },
 }: {
     params: { locale: string };
 }) {
-    //unstable_setRequestLocale(locale);
-    const t = useTranslations('ClubsPage');
-    const OpenStreetMap = dynamic(
-        () => import('@/app/Components/OpenStreetMap'),
-        {
-            ssr: false,
-        }
-    );
-    const [showMap, setShowMap] = useState(true);
 
     return (
         <div className="flex flex-col md:flex-col w-full justify-center md:justify-between items-center h-full overflow-auto pt-12 md:pl-[YourNavbarWidth]">
             <ClubsContent />
-            <br />
-            <button
-                onClick={() => setShowMap(!showMap)}
-                className={
-                    'py-2 px-4 md:py-3 md:px-7 flex flex-row justify-center rounded-3xl cursor-pointer items-center gap-3'
-                }
-                style={{ color: '#FFFFFF', backgroundColor: '#B6CF54' }}
-            >
-                {showMap ? t('clubs_menu_show_list') : t('clubs_menu_show_map')}
-            </button>
-            <br />
-            {showMap ? <p>{t('map_disclaimer')}</p> : null}
-            <br />
-            {showMap ? <OpenStreetMap /> : <ClubsList />}
         </div>
     );
 }

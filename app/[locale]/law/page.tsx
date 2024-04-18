7d1ddb73c3320b80388da0d77ffa0101ff62b1db
Hector Jones <jjones.hector@gmail.com>
2024-04-18 12:17:49 +0100

    [CAN-81] Adds metadata titles for each route Problem All routes were using the same metadata title 'Cannabis Clubs Berlin', bad for SEO Solution Added routes to each, along with the default being 'Cannabis Clubs Berlin', thanks to solutions at: https://www.builder.io/blog/routing-seo-metadata-nextjs-13 and for async requirements: https://next-intl-docs.vercel.app/docs/environments/server-client-components#async-components. Finally there was an issue for the clubs page, which had to be abstracted to a layout and then metadata added: https://github.com/vercel/next.js/discussions/51002 Note Also added extra localisations to the de.json and en.json for metadata, as well as correcting a non translated code in de.json Does 'Cannabis Clubs Berlin' work for german as well?

--- app/[locale]/law/page.tsx
+++ app/[locale]/law/page.tsx
@@ -1,10 +1,22 @@
+import { getTranslations } from 'next-intl/server';
 import LawContent from './law-content';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function About({ params: { locale } }: { params: { locale: string } }) {
+export const generateMetadata = async () => {
+    const t = await getTranslations('Metadata');
+    return {
+        title: t('law_title'),
+    };
+};
+
+export default function About({
+    params: { locale },
+}: {
+    params: { locale: string };
+}) {
     unstable_setRequestLocale(locale);
     return (
-        <div className='flex flex-col md:flex-row w-full justify-center md:justify-between items-center h-full'>
+        <div className="flex flex-col md:flex-row w-full justify-center md:justify-between items-center h-full">
             <LawContent />
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+    title: {
+        absolute: 'Home | Cannabis Clubs Berlin',
+    },
+};
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {

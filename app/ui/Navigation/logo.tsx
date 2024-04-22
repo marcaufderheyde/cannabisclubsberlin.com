5a668ae2834b200dba979f4b9e6796c31f6b3d71
Fabrizio <fabiecatinella@gmail.com>
2024-04-22 16:55:45 +0100

    [CAN-60] Refactor Logo to Accept onClick Problem - Logo doesn't close overlay when already on homepage
    Solution
    - Refactor logo to accept onClick
    - If on homepage and component is clicked, execute onClick
    
    Note

--- app/ui/Navigation/logo.tsx
+++ app/ui/Navigation/logo.tsx
@@ -1,14 +1,21 @@
 'use client';
 import { useLocale } from 'next-intl';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Logo({ onClick }: { onClick?: Function }) {
     const localActive = useLocale();
+    const pathname = usePathname();
+    const home = `/${localActive}`;
 
-    const handleClick: Function = onClick ? onClick : () => {};
+    const handleClick: Function = onClick
+        ? () => {
+              if (home == pathname) onClick();
+          }
+        : () => {};
 
     return (
-        <Link onClick={() => handleClick()} href={`/${localActive}`}>
+        <Link onClick={() => handleClick()} href={home}>
             <div className='font-bold text-xl'>
                 <p>Cannabis</p>
                 <p>Clubs</p>

f9fb7b23208a9696d808a066f826c05b5bae70c1
Fabrizio <fabiecatinella@gmail.com>
2024-04-22 16:46:44 +0100

    [CAn-60] Overlay Not Closing on Logo Click Mobile Problem - Overlay not closing on logo click
    Solution
    - Change the condition to match exact matches instead of regex matches
    
    Note
    - This will need to be changed in future to suit params

--- app/ui/Navigation/logo.tsx
+++ app/ui/Navigation/logo.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import { useLocale } from 'next-intl';
 import Link from 'next/link';
 
--- app/ui/Navigation/overlaynav.tsx
+++ app/ui/Navigation/overlaynav.tsx
@@ -22,7 +22,7 @@ export default function OverlayNav({
     useEffect(() => {
         const prevPathname = prevPathnameRef.current as string;
         if (prevPathname !== null && pathname !== null) {
-            if (!prevPathname.match(pathname)) {
+            if (prevPathname !== pathname) {
                 handleClick();
                 prevPathnameRef.current = pathname;
             }
@@ -34,21 +34,23 @@ export default function OverlayNav({
             onClick={(e) => {
                 handleClick();
             }}
-            className="fixed bg-[rgba(255,255,255,0.30)] z-10 min-w-full min-h-full backdrop-blur-md top-0 left-0 flex flex-col justify-start items-center"
+            className='fixed bg-[rgba(255,255,255,0.30)] z-10 min-w-full min-h-full backdrop-blur-md top-0 left-0 flex flex-col justify-start items-center'
         >
             <div
                 onClick={(e) => {
                     e.stopPropagation();
                 }}
-                className="bg-white rounded-[2rem] flex flex-col z-20 w-[90%] my-3 py-8 px-8 shadow-md"
+                className='bg-white rounded-[2rem] flex flex-col z-20 w-[90%] my-3 py-8 px-8 shadow-md'
             >
-                <div className="flex flex-row justify-between items-center text-[#B6CF54]">
-                    <Logo />
-                    <div onClick={() => handleClick()} className="px-4">
+                <div className='flex flex-row justify-between items-center text-[#B6CF54]'>
+                    <div>
+                        <Logo />
+                    </div>
+                    <div onClick={() => handleClick()} className='px-4'>
                         <Close color={'#828282'} />
                     </div>
                 </div>
-                <div className="flex flex-col justify-start items-start my-10 font-bold text-[1.6rem] text-[rgba(130,130,130,0.6)]">
+                <div className='flex flex-col justify-start items-start my-10 font-bold text-[1.6rem] text-[rgba(130,130,130,0.6)]'>
                     {links.map((link: LinkInfo) => {
                         return (
                             <Link
@@ -61,7 +63,7 @@ export default function OverlayNav({
                                           }
                                         : () => {}
                                 }
-                                className="min-w-full py-2"
+                                className='min-w-full py-2'
                                 key={'mobile_' + link.name}
                                 href={link.href}
                             >
@@ -70,7 +72,7 @@ export default function OverlayNav({
                         );
                     })}
                 </div>
-                <div className="flex flex-row justify-end items-center text-[#828282]">
+                <div className='flex flex-row justify-end items-center text-[#828282]'>
                     <LocalSwitcher />
                 </div>
             </div>

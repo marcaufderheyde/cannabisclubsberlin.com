c5df4268f171a30d35bb495afe2005dfc456f2bc
Marc Auf der Heyde <marcaufderheyde@gmail.com>
2024-04-22 20:30:52 +0200

    Merge develop into main for CAN-89, CAN-60 & CAN-80 (#73)
    * [CAN-89] Disable Scrolling on Mobile Nav
    Problem
    - When mobile overlay up, scrolling still enabled
    Solution
    - When component is mounted - disable scrolling on events
      - DOM Mouse Scrolls
      - Wheel Events (checked by searching property on div element)
      - Touch Move
      - Any Key Events
    - Package as a helper hook that can be reused on other component
    - Enable Scrolling when unmount the overlay
    - ASIDE: Passive Event listeners prevent blocking js code when scrolling
        - We need to tell the browser (if it is supported) to not block
        our event listeners as when need to check if the user is scrolling
    Note
    
    * [CAn-60] Overlay Not Closing on Logo Click Mobile
    Problem
    - Overlay not closing on logo click
    
    Solution
    - Change the condition to match exact matches instead of regex matches
    
    Note
    - This will need to be changed in future to suit params
    
    * [CAN-60] Refactor Logo To Accept OnClick
    Problem
    - Clicking on mobile logo doesn't close
    
    Solution
    - Enable onclick to be a prop
    - Pass close overlay to logo
    - Close
    Note
    
    * [CAN-60] Refactor Logo to Accept onClick
    Problem
    - Logo doesn't close overlay when already on homepage
    
    Solution
    - Refactor logo to accept onClick
    - If on homepage and component is clicked, execute onClick
    
    Note
    
    * [CAN-60] Refactor click handlers readability
    Problem
    - onClick doesn't really describe what is happening
    
    Solution
    - Refactor hamburger button from .jsx to .tsx
    - Refactor prop name on overlaynav form handleClick to closeOverlay
    - Refactor prop name hamburger button from handleClick to showOverlay
    
    Note
    
    * CAN-80 - Generate custom description and keywords for each club (#70)
    
    Problem: need custom meta for SEO
    
    Solution: painstakingly generate and paste in
    
    ---------
    
    Co-authored-by: Fabrizio <fabiecatinella@gmail.com>
    Co-authored-by: Fabrizio Catinella <45483794+fabie37@users.noreply.github.com>

--- app/ui/Navigation/overlaynav.tsx
+++ app/ui/Navigation/overlaynav.tsx
@@ -5,25 +5,27 @@ import LinkInfo from '@/app/ui/Navigation/linkinfo';
 import Logo from '@/app/ui/Navigation/logo';
 import Close from '@/app/ui/Navigation/close';
 import LocalSwitcher from './translation-switch';
-import { useEffect, useState, useRef, MutableRefObject } from 'react';
+import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import usePrevious from '@/app/helpers/usePrevious';
+import usePreventScrolling from '@/app/helpers/usePreventScrolling';
 
 export default function OverlayNav({
-    handleClick,
+    closeOverlay,
     links,
 }: {
-    handleClick: Function;
+    closeOverlay: Function;
     links: Array<LinkInfo>;
 }) {
     const pathname = usePathname();
     const prevPathnameRef = usePrevious(pathname);
+    const preventScroll = usePreventScrolling();
 
     useEffect(() => {
         const prevPathname = prevPathnameRef.current as string;
         if (prevPathname !== null && pathname !== null) {
-            if (!prevPathname.match(pathname)) {
-                handleClick();
+            if (prevPathname !== pathname) {
+                closeOverlay();
                 prevPathnameRef.current = pathname;
             }
         }
@@ -32,23 +34,23 @@ export default function OverlayNav({
     return (
         <div
             onClick={(e) => {
-                handleClick();
+                closeOverlay();
             }}
-            className="fixed bg-[rgba(255,255,255,0.30)] z-10 min-w-full min-h-full backdrop-blur-md top-0 left-0 flex flex-col justify-start items-center"
+            className='fixed bg-[rgba(255,255,255,0.30)] z-10 min-w-full min-h-full backdrop-blur-md top-0 left-0 flex flex-col justify-start items-center'
         >
             <div
                 onClick={(e) => {
                     e.stopPropagation();
                 }}
-                className="bg-white rounded-[2rem] flex flex-col z-20 w-[90%] my-3 py-8 px-8 shadow-md"
+                className='bg-white rounded-[2rem] flex flex-col z-20 w-[90%] my-3 py-8 px-8 shadow-md'
             >
-                <div className="flex flex-row justify-between items-center text-[#B6CF54]">
-                    <Logo />
-                    <div onClick={() => handleClick()} className="px-4">
+                <div className='flex flex-row justify-between items-center text-[#B6CF54]'>
+                    <Logo onClick={() => closeOverlay()} />
+                    <div onClick={() => closeOverlay()} className='px-4'>
                         <Close color={'#828282'} />
                     </div>
                 </div>
-                <div className="flex flex-col justify-start items-start my-10 font-bold text-[1.6rem] text-[rgba(130,130,130,0.6)]">
+                <div className='flex flex-col justify-start items-start my-10 font-bold text-[1.6rem] text-[rgba(130,130,130,0.6)]'>
                     {links.map((link: LinkInfo) => {
                         return (
                             <Link
@@ -57,11 +59,11 @@ export default function OverlayNav({
                                         link.href as string
                                     )
                                         ? () => {
-                                              handleClick();
+                                              closeOverlay();
                                           }
                                         : () => {}
                                 }
-                                className="min-w-full py-2"
+                                className='min-w-full py-2'
                                 key={'mobile_' + link.name}
                                 href={link.href}
                             >
@@ -70,7 +72,7 @@ export default function OverlayNav({
                         );
                     })}
                 </div>
-                <div className="flex flex-row justify-end items-center text-[#828282]">
+                <div className='flex flex-row justify-end items-center text-[#828282]'>
                     <LocalSwitcher />
                 </div>
             </div>

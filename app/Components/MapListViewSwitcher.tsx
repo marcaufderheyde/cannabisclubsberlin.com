0b8144ac2d0154ae2066e812cbf585c0ccdd63ad
Fabrizio <fabiecatinella@gmail.com>
2024-06-25 16:58:20 +0100

    [CAN-63, CAN-99] Position Switcher to right Problem - On desktop, popup cards block Switcher
    Solution
    - On desktop, change left-0 to right-0 for absolute
    - On mobile, want to maintain spacing above list
      - Remove absolute
      - Encapsulate component with parent flexbox to perform alignment
      - Make parent width 100VW instead of 100% so scrollbar does not affect
      positioning
    
    Note

--- app/Components/MapListViewSwitcher.tsx
+++ app/Components/MapListViewSwitcher.tsx
@@ -18,35 +18,39 @@ function MapListViewSwitcher({ showMap, setShowMap }: Props) {
         ? 'bg-gray-200 text-neutral-400'
         : 'bg-white text-black';
     const mapListViewSwitcherPosition = showMap
-        ? 'absolute top-[var(--navbar-height)] left-0'
-        : 'right-0';
+        ? 'absolute top-[var(--navbar-height)] right-0'
+        : 'relative';
     return (
-        <div
-            className={
-                'inline-flex ' +
-                mapListViewSwitcherPosition +
-                ' z-[998] lg:m-8 lg:ml-20 m-4 rounded-3xl shadow-md'
-            }
-        >
-            <div className={'z-[999] rounded-l-3xl ' + mapButtonBackground}>
-                <button
-                    onClick={() => setShowMap(true)}
-                    className={
-                        'flex z-[999] cursor-pointer items-center p-2 lg:p-4'
-                    }
+        <div className='w-[100vw] flex align-middle justify-end'>
+            <div
+                className={
+                    'inline-flex ' +
+                    mapListViewSwitcherPosition +
+                    ' z-[998] lg:m-8 lg:ml-20 m-4 rounded-3xl shadow-md'
+                }
+            >
+                <div className={'z-[999] rounded-l-3xl ' + mapButtonBackground}>
+                    <button
+                        onClick={() => setShowMap(true)}
+                        className={
+                            'flex z-[999] cursor-pointer items-center p-2 lg:p-4'
+                        }
+                    >
+                        {t('clubs_menu_show_map')}
+                    </button>
+                </div>
+                <div
+                    className={'z-[999] rounded-r-3xl ' + listButtonBackground}
                 >
-                    {t('clubs_menu_show_map')}
-                </button>
-            </div>
-            <div className={'z-[999] rounded-r-3xl ' + listButtonBackground}>
-                <button
-                    onClick={() => setShowMap(false)}
-                    className={
-                        'flex z-[999] cursor-pointer items-center p-2 lg:p-4'
-                    }
-                >
-                    {t('clubs_menu_show_list')}
-                </button>
+                    <button
+                        onClick={() => setShowMap(false)}
+                        className={
+                            'flex z-[999] cursor-pointer items-center p-2 lg:p-4'
+                        }
+                    >
+                        {t('clubs_menu_show_list')}
+                    </button>
+                </div>
             </div>
         </div>
     );

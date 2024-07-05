f23eebe1d78cd83d09ee0d978922d783443ef7a2
Hector Jones <jjones.hector@gmail.com>
2024-07-05 21:38:55 +0100

    [CAN-108] Removes more console logs Problem Solution Note

--- app/Components/OpenStreetMap.tsx
+++ app/Components/OpenStreetMap.tsx
@@ -85,7 +85,6 @@ export default function OpenStreetMap(props: OpenStreetMapProps) {
                 props.isDesktopMap
             );
         }
-        console.log(clubIndex);
     }, [clubIndex]);
 
     return (
@@ -113,9 +112,9 @@ export default function OpenStreetMap(props: OpenStreetMapProps) {
                     style={{ height: '100%', width: '100%' }}
                     ref={setMap}
                 >
-                    <ZoomControl position='bottomright' />
+                    <ZoomControl position="bottomright" />
                     <TileLayer
-                        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
                     {clubs.map((club, index) => (

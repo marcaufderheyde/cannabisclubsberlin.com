99b7459542b5cd3aa375388db7746fe30560d403
Fabrizio <fabiecatinella@gmail.com>
2024-07-08 21:11:47 +0100

    [CAN-108] Swipeable Card Deck Problem - Ensure Cards can swipe left and right and move without glitches
    Solution
    - Implement logic to hand swipe actions
    - Include new state to record when cards are active
    - Iterate over an array of pre rendered cards to display
    Note

--- app/Components/SwipeableClubCard.tsx
+++ app/Components/SwipeableClubCard.tsx
@@ -1,50 +1,80 @@
 'use client';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import Swipeable from './Swipeable';
 import { Club } from '../helpers/clubsListContent';
 import { TouchDetails } from './Swipeable';
+import { Position } from './SwipeableDeck';
 
 export default function SwipeableClubCard({
     club,
     index,
+    startPosition,
+    startScale,
+    onRightSwipe,
+    onLeftSwipe,
+    zHeight,
 }: {
     readonly club: Club;
     readonly index: number;
+    readonly startPosition: Position;
+    readonly startScale: number;
+    onRightSwipe?: Function;
+    onLeftSwipe?: Function;
+    readonly zHeight: number;
 }) {
     const [position, setPosition] = useState({
-        y: 200,
-        x: 60,
+        y: startPosition.y,
+        x: startPosition.x,
     });
-    const [transformDuration, setTranformDuration] = useState(0);
+
+    const [transformDuration, setTranformDuration] = useState(0.2);
+    const [scale, setScale] = useState(startScale);
+
+    useEffect(() => {
+        setTranformDuration(0.2);
+        setScale(startScale);
+        setPosition(startPosition);
+    }, [startPosition]);
+
     const swipeAnimation = (touchDetails: TouchDetails) => {
         setTranformDuration(0);
         setPosition((prevPosition) => ({
             ...prevPosition,
-            x: 60 + touchDetails.distX,
+            x: startPosition.x + touchDetails.distX,
         }));
     };
 
     const swipeOnCancel = () => {
         setTranformDuration(0.2);
         setPosition({
-            y: 200,
-            x: 60,
+            y: startPosition.y,
+            x: startPosition.x,
         });
     };
 
     const translationStyling: React.CSSProperties = {
-        transform: `translate(${position.x}px, ${position.y}px)`,
+        transform: `translate(${position.x}px, ${position.y}px) scale(${scale})`,
         transition: `transform ${transformDuration}s`,
+        zIndex: `${zHeight}`,
     };
 
     return (
-        <Swipeable duringSwipe={swipeAnimation} onCancel={swipeOnCancel}>
+        <Swipeable
+            duringSwipe={
+                onLeftSwipe && onRightSwipe ? swipeAnimation : () => {}
+            }
+            onCancel={swipeOnCancel}
+            onLeftSwipe={onLeftSwipe}
+            onRightSwipe={onRightSwipe}
+        >
             <div
                 style={translationStyling}
                 className={
                     'absolute bg-white w-[250px] h-[500px] bottom-0 rounded-[3em] shadow-lg z-1 pointer-events-auto'
                 }
-            ></div>
+            >
+                {index}
+            </div>
         </Swipeable>
     );
 }
--- app/Components/SwipeableDeck.tsx
+++ app/Components/SwipeableDeck.tsx
@@ -1,6 +1,21 @@
 'use client';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState, useMemo } from 'react';
 import useWindowSize from '../helpers/useWindowSize';
+import mod from '../helpers/mod';
+
+export interface Position {
+    x: number;
+    y: number;
+}
+
+export interface CardInfo {
+    position: Position;
+    scale: number;
+    index: number;
+    onLeftSwipe: Function;
+    onRightSwipe: Function;
+    zHeight: number;
+}
 
 export default function SwipeableDeck<T>({
     items,
@@ -16,14 +31,93 @@ export default function SwipeableDeck<T>({
     onLeftSwipe: Function;
 }) {
     const preRenderCount = Math.min(5, items.length);
+    const [shownCards, setShownCards] = useState<CardInfo[]>([]);
+
+    const inBetweenBounds = (
+        start: number,
+        end: number,
+        numberItems: number,
+        index: number
+    ) => {
+        start %= numberItems;
+        end %= numberItems;
+        index %= numberItems;
+
+        if (start <= end) {
+            return start <= index && index <= end;
+        } else {
+            return index >= start || index <= end;
+        }
+    };
+
+    const calcDistanceToMiddle = (
+        start: number,
+        end: number,
+        middle: number,
+        numberItems: number,
+        index: number
+    ) => {
+        if (end < start) {
+            if (index <= end && index < start)
+                return numberItems + index - middle;
+            else middle - index;
+        }
+        return index - middle;
+    };
+
+    useEffect(() => {
+        const minIndex = mod(
+            currentIndex - (preRenderCount - 1) / 2,
+            items.length
+        );
+        const maxIndex = mod(
+            currentIndex + (preRenderCount - 1) / 2,
+            items.length
+        );
+
+        setShownCards(() => {
+            let i = minIndex;
+            const cards = [];
+            let distance = -(preRenderCount - 1) / 2;
+            while (inBetweenBounds(minIndex, maxIndex, items.length, i)) {
+                const startPosition = {
+                    x: 60 + distance * 250,
+                    y: 200,
+                };
+                const scale = currentIndex == i ? 1.0 : 0.6;
+                const cardInfo = {
+                    position: startPosition,
+                    scale: scale,
+                    onLeftSwipe: i == currentIndex ? onLeftSwipe : () => {},
+                    onRightSwipe: i == currentIndex ? onRightSwipe : () => {},
+                    index: i,
+                    zHeight: i == currentIndex ? 1 : -distance,
+                };
+                cards.push(cardInfo);
+
+                i = mod(i + 1, items.length);
+                distance += 1;
+            }
+
+            return cards;
+        });
+    }, [currentIndex]);
 
     return (
         <div className='absolute bottom-0 z-[3000] w-full h-full bg-[rgba(233,246,166,0.3)] pointer-events-[fill] overflow-hidden pointer-events-none'>
-            {/* <div className='absolute bg-white w-[250px] h-[500px] bottom-0 rounded-[3em] translate-y-[+200px] translate-x-[-80px] scale-[0.45] shadow-lg z-[-2] '></div> */}
-            <div className='absolute bg-white w-[250px] h-[500px] bottom-0 rounded-[3em] translate-y-[+200px] translate-x-[-150px] shadow-lg scale-[0.60] z-[-1]'></div>
-            <Card />
-            <div className='absolute bg-white w-[250px] h-[500px] bottom-0 rounded-[3em] translate-y-[+200px] translate-x-[+270px] scale-[0.60] shadow-lg z-[-1]'></div>
-            {/* <div className='absolute bg-white w-[250px] h-[500px] bottom-0 rounded-[3em] translate-y-[+200px] translate-x-[+200px] scale-[0.45] shadow-lg z-[-2]'></div> */}
+            {shownCards.map((card) => {
+                return (
+                    <Card
+                        startPosition={card.position}
+                        startScale={card.scale}
+                        index={card.index}
+                        key={card.index}
+                        onLeftSwipe={card.onLeftSwipe}
+                        onRightSwipe={card.onRightSwipe}
+                        zHeight={card.zHeight}
+                    />
+                );
+            })}
         </div>
     );
 }

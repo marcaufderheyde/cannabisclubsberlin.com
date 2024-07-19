8018cf7a749c54700f9f12a413032c159aa64cd0
Marc Auf der Heyde <marcaufderheyde@gmail.com>
2024-07-19 08:52:17 +0200

    HOTFIX - use google analytics package (#98)
    Co-authored-by: marcaufderheyde <marc.auf-der-heyde@deutschebahn.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Inter } from 'next/font/google';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
 import './globals.css';
-import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleTagManager } from '@next/third-parties/google';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,29 +26,8 @@ export default function RootLayout({
 }>) {
     return (
         <html lang={locale}>
-            <head>
-                <Script id="gtm-script" strategy="afterInteractive">
-                    {`
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-PBKDVXT9');
-                    `}
-                </Script>
-            </head>
-            <body className={inter.className}>
-                {/* Google Tag Manager (noscript) */}
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-PBKDVXT9"
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    ></iframe>
-                </noscript>
-                {children}
-            </body>
+            <body className={inter.className}>{children}</body>
+            <GoogleTagManager gtmId="GTM-PBKDVXT9" /> // METRIC ID
         </html>
     );
 }

7c916aa9971e5204b51fc74ae2e2f60c192f6304
Fabrizio <fabiecatinella@gmail.com>
2024-07-19 15:24:49 +0100

    [CAN-108] Center cards depending on windowSize Problem - Cards did not center based on window size
    Solution
    - useWindowSize hook gets the window size
    - calcStartPosition - calculates where each card should be relative to
    window size
    - Sometimes useWindowSize is undefined, handle for case.
    Note

--- app/Components/SwipeableDeck.tsx
+++ app/Components/SwipeableDeck.tsx
@@ -40,6 +40,9 @@ export default function SwipeableDeck<T>({
     onRightSwipe: Function;
     onLeftSwipe: Function;
 }) {
+    const windowSize = useWindowSize();
+    const cardWidth = 250;
+
     const preRenderCount = Math.min(5, items.length);
     const [shownCards, setShownCards] = useState<CardInfo[]>([]);
 
@@ -75,6 +78,21 @@ export default function SwipeableDeck<T>({
         return index - middle;
     };
 
+    const calcStartPosition = (distance: number) => {
+        if (windowSize == undefined) {
+            return { x: 0, y: 500 };
+        } else {
+            const middleOfScreen = windowSize.width / 2;
+            const halfCardSize = cardWidth / 2;
+            const spaceBetweenCards = 0;
+            const x =
+                middleOfScreen -
+                halfCardSize +
+                distance * (cardWidth + spaceBetweenCards);
+            return { x: x, y: 200 };
+        }
+    };
+
     useEffect(() => {
         const minIndex = mod(
             currentIndex - (preRenderCount - 1) / 2,
@@ -90,10 +108,8 @@ export default function SwipeableDeck<T>({
             const cards = [];
             let distance = -(preRenderCount - 1) / 2;
             while (inBetweenBounds(minIndex, maxIndex, items.length, i)) {
-                const startPosition = {
-                    x: 60 + distance * 250,
-                    y: 200,
-                };
+                const startPosition = calcStartPosition(distance);
+                console.log(startPosition);
                 const scale = currentIndex == i ? 1.0 : 0.6;
                 const cardInfo = {
                     position: startPosition,
@@ -113,26 +129,27 @@ export default function SwipeableDeck<T>({
 
             return cards;
         });
-    }, [currentIndex]);
+    }, [currentIndex, windowSize]);
 
     return (
-        <div className="lg:hidden absolute bottom-0 z-[3000] w-full h-full pointer-events-[fill] overflow-hidden pointer-events-none">
-            {shownCards.map((card) => {
-                return (
-                    <Card
-                        club={items[card.index]}
-                        startPosition={card.position}
-                        startScale={card.scale}
-                        index={card.index}
-                        key={card.index}
-                        onClose={card.onClose}
-                        onLeftSwipe={card.onLeftSwipe}
-                        onRightSwipe={card.onRightSwipe}
-                        zHeight={card.zHeight}
-                        canSwipe={card.canSwipe}
-                    />
-                );
-            })}
+        <div className='lg:hidden absolute bottom-0 z-[3000] w-full h-full pointer-events-[fill] overflow-hidden pointer-events-none'>
+            {windowSize &&
+                shownCards.map((card) => {
+                    return (
+                        <Card
+                            club={items[card.index]}
+                            startPosition={card.position}
+                            startScale={card.scale}
+                            index={card.index}
+                            key={card.index}
+                            onClose={card.onClose}
+                            onLeftSwipe={card.onLeftSwipe}
+                            onRightSwipe={card.onRightSwipe}
+                            zHeight={card.zHeight}
+                            canSwipe={card.canSwipe}
+                        />
+                    );
+                })}
         </div>
     );
 }

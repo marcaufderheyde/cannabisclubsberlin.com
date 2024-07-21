861bddd0e004f031280250c4a678de23bbb22d5e
Hector Jones <jjones.hector@gmail.com>
2024-07-21 16:47:51 +0100

    [CAN-111] Initialises experimental branch for animations on cards Problem Solution Note

--- app/Components/SwipeableClubCard.tsx
+++ app/Components/SwipeableClubCard.tsx
@@ -115,51 +115,51 @@ export default function SwipeableClubCard({
             onDownSwipeClose={onDownSwipeClose}
             // onUpSwipeClose={onUpSwipeClose}
         >
-            {/* <motion.div
+            <motion.div
                 layoutId={`club-${club.slug}`}
                 initial={{ height: '100px' }}
                 animate={{ height: '800px', width: '400px' }}
                 transition={{ duration: 0.3, ease: 'easeInOut' }}
-            > */}
-            <div
-                style={translationStyling}
-                className={
-                    'overscroll-contained absolute bg-white w-[250px] h-[450px] bottom-0 rounded-[3em] shadow-lg z-1 pointer-events-auto'
-                }
             >
-                <div className="relative overscroll-contained">
+                <div
+                    style={translationStyling}
+                    className={
+                        'overscroll-contained absolute bg-white w-[250px] h-[450px] bottom-0 rounded-[3em] shadow-lg z-1 pointer-events-auto'
+                    }
+                >
                     <div className="relative overscroll-contained">
-                        <Image
-                            src={club.imageUrl}
-                            alt={`${club.name} Club Picture`}
-                            width={300}
-                            height={300}
-                            className="absolute top-[-20px] h-[100px] object-contain p-[5px] rounded-[8px]"
-                        />
-                    </div>
-                    <div className="absolute top-[80px] max-h-[calc(100%-80px)]">
-                        <h1
-                            className={
-                                'text-black font-bold md:text-[4rem] text-center m-2'
-                            }
-                            onClick={handleExpandToClubPage}
-                        >
-                            {club.name}
-                        </h1>
-                        <div className="mx-2 flex flex-wrap justify-center">
-                            {club.offerings?.map((offering: string) => (
-                                <span
-                                    key={offering}
-                                    className="bg-blue-100 text-blue-800 text-xs font-medium me-2 p-0.5 inline-block overflow-hidden text-center rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400"
-                                >
-                                    {offering}
-                                </span>
-                            ))}
+                        <div className="relative overscroll-contained">
+                            <Image
+                                src={club.imageUrl}
+                                alt={`${club.name} Club Picture`}
+                                width={300}
+                                height={300}
+                                className="absolute top-[-20px] h-[100px] object-contain p-[5px] rounded-[8px]"
+                            />
+                        </div>
+                        <div className="absolute top-[80px] max-h-[calc(100%-80px)]">
+                            <h1
+                                className={
+                                    'text-black font-bold md:text-[4rem] text-center m-2'
+                                }
+                                onClick={handleExpandToClubPage}
+                            >
+                                {club.name}
+                            </h1>
+                            <div className="mx-2 flex flex-wrap justify-center">
+                                {club.offerings?.map((offering: string) => (
+                                    <span
+                                        key={offering}
+                                        className="bg-blue-100 text-blue-800 text-xs font-medium me-2 p-0.5 inline-block overflow-hidden text-center rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400"
+                                    >
+                                        {offering}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {/* </motion.div> */}
+            </motion.div>
         </Swipeable>
     );
 }

2f8696cd0fe00a50bc6d74c635bb21007cb18de4
Fabrizio <fabiecatinella@gmail.com>
2024-08-19 12:40:23 +0100

    [CAN-127] Refactor: Remove Unused code in clubs content Problem - Lots couple of unused variables - showMap state is no longer needed as we always show map Solution - Removed code from codebase - we can always look back at commits if we need it again Note

--- app/[locale]/(withoutheaderfooter)/clubs/clubs-content.tsx
+++ app/[locale]/(withoutheaderfooter)/clubs/clubs-content.tsx
@@ -12,58 +12,34 @@ const OpenStreetMap = dynamic(() => import('@/app/Components/OpenStreetMap'), {
 });
 
 export default function ClubsContent() {
-    const [showMap, setShowMap] = useState(true);
     const [selectedClubFromList, setSelectedClubFromList] = useState<Club>();
     const [showHRFilter, setShowHRFilter] = useState(false);
 
     return (
         <div>
             <Navbar isOnMap={true} />
-            {showMap ? (
-                <div>
-                    <div className='hidden lg:flex'>
-                        <OpenStreetMap
-                            showHRInfo={showHRFilter}
-                            isDesktopMap={true}
-                        />
-                        <MapListFilterSwitcher
-                            showMap={showMap}
-                            showHRFilter={showHRFilter}
-                            setShowHRFilter={setShowHRFilter}
-                        />{' '}
-                    </div>
-                    <div className='lg:hidden flex'>
-                        <OpenStreetMap
-                            showHRInfo={showHRFilter}
-                            isDesktopMap={false}
-                        />
-                        <MapListFilterSwitcher
-                            showMap={showMap}
-                            showHRFilter={showHRFilter}
-                            setShowHRFilter={setShowHRFilter}
-                        />{' '}
-                    </div>
-                </div>
-            ) : (
-                <div className='md:hidden absolute top-[var(--navbar-height-mobile)] md:top-[var(--navbar-height)] left-0'>
+            <div>
+                <div className='hidden lg:flex'>
+                    <OpenStreetMap
+                        showHRInfo={showHRFilter}
+                        isDesktopMap={true}
+                    />
                     <MapListFilterSwitcher
-                        showMap={showMap}
                         showHRFilter={showHRFilter}
                         setShowHRFilter={setShowHRFilter}
                     />{' '}
-                    <MapListViewSwitcher
-                        showMap={showMap}
-                        setShowMap={setShowMap}
-                        setShowHRFilter={setShowHRFilter}
-                    />
-                    <MobileClubList
-                        showHRInfo={showHRFilter}
-                        clubClickedFromList={() => {
-                            setSelectedClubFromList;
-                        }}
-                    />
                 </div>
-            )}
+                <div className='lg:hidden flex'>
+                    <OpenStreetMap
+                        showHRInfo={showHRFilter}
+                        isDesktopMap={false}
+                    />
+                    <MapListFilterSwitcher
+                        showHRFilter={showHRFilter}
+                        setShowHRFilter={setShowHRFilter}
+                    />{' '}
+                </div>
+            </div>
         </div>
     );
 }

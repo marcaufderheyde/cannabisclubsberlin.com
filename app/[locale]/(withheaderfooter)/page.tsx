11a64cb3fc204b3aa36ce4f83ec784af18260ffb
marcaufderheyde <marc.auf-der-heyde@deutschebahn.com>
2025-06-17 18:56:10 +0200

    Subject - Club contacted me to update their listing
    Problem - can't have stale data when they come to us!
    
    Solution - update the page
    
    Note - nothing

--- app/[locale]/(withheaderfooter)/page.tsx
+++ app/[locale]/(withheaderfooter)/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import Background from '@/app/components/Home/Background';
 import HomeContent from '../../components/Home/Home-content';
+import { generateSlug } from '@/app/helpers/clubsListContent';
 
 export default function IndexPage() {
+    console.log(generateSlug('Power-Flower Cannabis Club e.V.'));
     return (
         <>
             <Background />
-            <div className='flex flex-col md:flex-row w-full justify-center md:justify-between items-center'>
+            <div className="flex flex-col md:flex-row w-full justify-center md:justify-between items-center">
                 <HomeContent />
             </div>
         </>
